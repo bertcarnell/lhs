a8d12597a30255044cf04c6f85bad2db30911124
bertcarnell <bertcarnell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-28 17:10:16 +0000

    Changed makefile to work with subversion
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lhs@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/tests/doRUnit.r
+++ trunk/tests/doRUnit.r
@@ -10,7 +10,7 @@ if(require("RUnit", quietly=TRUE)) {
   if(Sys.getenv("RCMDCHECK") == "FALSE") {
     ## Path to unit tests for standalone running under Makefile (not R CMD check)
     ## PKG/tests/../inst/unitTests
-    path <- file.path(getwd(), "..", "inst", "unitTests")
+    path <- file.path(getwd(), "..", "tests", "unitTests")
   } else {
     ## Path to unit tests for R CMD check
     ## PKG.Rcheck/tests/../PKG/unitTests
