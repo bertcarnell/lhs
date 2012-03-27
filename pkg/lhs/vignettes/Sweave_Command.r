4536bf6b96d80cd7e1f5f374b103f97d870dbda9
bertcarnell <bertcarnell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-27 02:17:00 +0000

    Changing package to correct R CMD check error for R-2.15.0
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lhs@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lhs/vignettes/Sweave_Command.r
+++
@@ -1,9 +0,0 @@
-sweavePath <- file.path("C:","Documents and Settings","carnellr","My Documents",
-                        "R Packages","lhs","inst","doc")
-outputPath <- file.path("C:","Documents and Settings","carnellr","My Documents",
-                        "R Packages","lhs","vignettes")
-
-Sweave(file.path(sweavePath, "augmentLHS_Example.Stex"),
-       syntax=SweaveSyntaxLatex,
-       output=file.path(outputPath, "augmentLHS_Example.tex"))
-
