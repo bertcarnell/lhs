2d037296a7456874689f201360e7139abc83c11f
bertcarnell <bertcarnell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-10 02:18:39 +0000

    Fixed a bug in the braces in the roxygen2 comments
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lhs@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/oalhs/R/createOA.R
+++ pkg/oalhs/R/createOA.R
@@ -9,13 +9,13 @@
 #' @examples A <- createBose(3, 3, FALSE)
 #' B <- createBose(5, 4, TRUE)
 #' @seealso Other methods to create orthogonal arrays 
-#' \code(\link{createBush}), 
-#' \code(\link{creatBoseBush}), 
-#' \code(\link{createAddelKemp}), 
-#' \code(\link{createAddelKemp3}), 
-#' \code(\link{createAddelKempN}), 
-#' \code(\link{createBusht}), 
-#' \code(\link{createBoseBushl})
+#' \code{\link{createBush}}, 
+#' \code{\link{creatBoseBush}}, 
+#' \code{\link{createAddelKemp}}, 
+#' \code{\link{createAddelKemp3}}, 
+#' \code{\link{createAddelKempN}}, 
+#' \code{\link{createBusht}}, 
+#' \code{\link{createBoseBushl}}
 createBose <- function(q, ncol, bRandom)
 {
   return (.Call("oa_type1", "bose", as.integer(q), as.integer(ncol), 
@@ -32,13 +32,13 @@ createBose <- function(q, ncol, bRandom)
 #' @examples A <- createBoseBush(4, 3, FALSE)
 #' B <- createBoseBush(3, 3, TRUE)
 #' @seealso Other methods to create orthogonal arrays 
-#' \code(\link{createBush}), 
-#' \code(\link{creatBose}), 
-#' \code(\link{createAddelKemp}), 
-#' \code(\link{createAddelKemp3}), 
-#' \code(\link{createAddelKempN}), 
-#' \code(\link{createBusht}), 
-#' \code(\link{createBoseBushl})
+#' \code{\link{createBush}}, 
+#' \code{\link{creatBose}}, 
+#' \code{\link{createAddelKemp}}, 
+#' \code{\link{createAddelKemp3}}, 
+#' \code{\link{createAddelKempN}}, 
+#' \code{\link{createBusht}}, 
+#' \code{\link{createBoseBushl}}
 createBoseBush <- function(q, ncol, bRandom)
 {
   return (.Call("oa_type1", "bosebush", as.integer(q), as.integer(ncol), 
@@ -54,13 +54,13 @@ createBoseBush <- function(q, ncol, bRandom)
 #' @export
 #' @examples A <- createBush(3, 3, FALSE)
 #' B <- createBush(4, 5, TRUE)
-#' \code(\link{createBoseBush}), 
-#' \code(\link{creatBose}), 
-#' \code(\link{createAddelKemp}), 
-#' \code(\link{createAddelKemp3}), 
-#' \code(\link{createAddelKempN}), 
-#' \code(\link{createBusht}), 
-#' \code(\link{createBoseBushl})
+#' \code{\link{createBoseBush}}, 
+#' \code{\link{creatBose}}, 
+#' \code{\link{createAddelKemp}}, 
+#' \code{\link{createAddelKemp3}}, 
+#' \code{\link{createAddelKempN}}, 
+#' \code{\link{createBusht}}, 
+#' \code{\link{createBoseBushl}}
 createBush <- function(q, ncol, bRandom)
 {
   return (.Call("oa_type1", "bush", as.integer(q), as.integer(ncol),  
@@ -76,13 +76,13 @@ createBush <- function(q, ncol, bRandom)
 #' @export
 #' @examples A <- createAddelKemp(3, 3, TRUE)
 #' B <- createAddelKemp(3, 5, FALSE)
-#' \code(\link{createBoseBush}), 
-#' \code(\link{creatBose}), 
-#' \code(\link{createBush}), 
-#' \code(\link{createAddelKemp3}), 
-#' \code(\link{createAddelKempN}), 
-#' \code(\link{createBusht}), 
-#' \code(\link{createBoseBushl})
+#' \code{\link{createBoseBush}}, 
+#' \code{\link{creatBose}}, 
+#' \code{\link{createBush}}, 
+#' \code{\link{createAddelKemp3}}, 
+#' \code{\link{createAddelKempN}}, 
+#' \code{\link{createBusht}}, 
+#' \code{\link{createBoseBushl}}
 createAddelKemp <- function(q, ncol, bRandom)
 {
   return (.Call("oa_type1", "addelkemp", as.integer(q), as.integer(ncol),  
@@ -99,13 +99,13 @@ createAddelKemp <- function(q, ncol, bRandom)
 #' @export
 #' @examples A <- createAddelKemp3(3, 3, TRUE)
 #' B <- createAddelKemp3(3, 5, FALSE)
-#' \code(\link{createBoseBush}), 
-#' \code(\link{creatBose}), 
-#' \code(\link{createBush}), 
-#' \code(\link{createAddelKemp}), 
-#' \code(\link{createAddelKempN}), 
-#' \code(\link{createBusht}), 
-#' \code(\link{createBoseBushl})
+#' \code{\link{createBoseBush}}, 
+#' \code{\link{creatBose}}, 
+#' \code{\link{createBush}}, 
+#' \code{\link{createAddelKemp}}, 
+#' \code{\link{createAddelKempN}}, 
+#' \code{\link{createBusht}}, 
+#' \code{\link{createBoseBushl}}
 createAddelKemp3 <- function(q, ncol, bRandom)
 {
   return (.Call("oa_type1", "addelkemp3", as.integer(q), as.integer(ncol), 
@@ -123,13 +123,13 @@ createAddelKemp3 <- function(q, ncol, bRandom)
 #' @examples A <- createBusht(3, 4, 2, TRUE)
 #' B <- createBusht(3, 4, 3, FALSE)
 #' G <- createBusht(3, 4, 4, TRUE)
-#' \code(\link{createBoseBush}), 
-#' \code(\link{creatBose}), 
-#' \code(\link{createBush}), 
-#' \code(\link{createAddelKemp}), 
-#' \code(\link{createAddelKempN}), 
-#' \code(\link{createAddelKemp3}), 
-#' \code(\link{createBoseBushl})
+#' \code{\link{createBoseBush}}, 
+#' \code{\link{creatBose}}, 
+#' \code{\link{createBush}}, 
+#' \code{\link{createAddelKemp}}, 
+#' \code{\link{createAddelKempN}}, 
+#' \code{\link{createAddelKemp3}}, 
+#' \code{\link{createBoseBushl}}
 createBusht <- function(q, ncol, strength, bRandom)
 {
   return (.Call("oa_type2", "busht", as.integer(strength), as.integer(q), 
@@ -145,13 +145,13 @@ createBusht <- function(q, ncol, strength, bRandom)
 #' @export
 #' @examples A <- createBoseBushl(3, 3, 3, TRUE)
 #' B <- createBoseBushl(4, 4, 16, TRUE)
-#' \code(\link{createBoseBush}), 
-#' \code(\link{creatBose}), 
-#' \code(\link{createBush}), 
-#' \code(\link{createAddelKemp}), 
-#' \code(\link{createAddelKempN}), 
-#' \code(\link{createAddelKemp3}), 
-#' \code(\link{createBusht})
+#' \code{\link{createBoseBush}}, 
+#' \code{\link{creatBose}}, 
+#' \code{\link{createBush}}, 
+#' \code{\link{createAddelKemp}}, 
+#' \code{\link{createAddelKempN}}, 
+#' \code{\link{createAddelKemp3}}, 
+#' \code{\link{createBusht}}
 createBoseBushl <- function(q, ncol, lambda, bRandom)
 {
   return (.Call("oa_type2", "bosebushl", as.integer(lambda), as.integer(q), 
@@ -168,13 +168,13 @@ createBoseBushl <- function(q, ncol, lambda, bRandom)
 #' @export
 #' @examples A <- createAddelKempN(3, 4, 3, TRUE)
 #' B <- createAddelKempN(3, 4, 4, TRUE)
-#' \code(\link{createBoseBush}), 
-#' \code(\link{creatBose}), 
-#' \code(\link{createBush}), 
-#' \code(\link{createAddelKemp}), 
-#' \code(\link{createBusht}), 
-#' \code(\link{createAddelKemp3}), 
-#' \code(\link{createBoseBushl})
+#' \code{\link{createBoseBush}}, 
+#' \code{\link{creatBose}}, 
+#' \code{\link{createBush}}, 
+#' \code{\link{createAddelKemp}}, 
+#' \code{\link{createBusht}}, 
+#' \code{\link{createAddelKemp3}}, 
+#' \code{\link{createBoseBushl}}
 createAddelKempN <- function(q, ncol, exponent, bRandom)
 {
   return (.Call("oa_type2", "addelkempn", as.integer(exponent), as.integer(q), 
