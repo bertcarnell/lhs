50b9666195e053bed88dcf29286bedd75c8ebfa9
Rob Carnell <bertcarnell@gmail.com>
2019-01-27 14:56:03 -0500

    Continuing to work on the reverse dependencies

---
+++ rev_dep_check.R
@@ -0,0 +1,39 @@
+# get lhs reverse dependencies
+
+require(revdepcheck)
+require(devtools)
+lhs_rev_depends <- devtools::revdep(pkg = "lhs", dependencies = "Depends")
+lhs_rev_imports <- devtools::revdep(pkg = "lhs", dependencies = "Imports")
+lhs_rev_suggests <- devtools::revdep(pkg = "lhs", dependencies = "Suggests")
+
+# get all the necessary packages that they depend on
+lhs_rev_depends_req <- lapply(lhs_rev_depends, function(x) tools::package_dependencies(packages = x))
+lhs_rev_imports_req <- lapply(lhs_rev_imports, function(x) tools::package_dependencies(packages = x))
+lhs_rev_suggests_req <- lapply(lhs_rev_suggests, function(x) tools::package_dependencies(packages = x))
+
+rev_dep_req_install <- function(rev_req)
+{
+  # install if not done already
+  rev_depends_ureq <- unique(unlist(rev_req))
+  for (i in seq_along(rev_depends_ureq))
+  {
+    # don't need to check or install lhs
+    if (rev_depends_ureq[i] == "lhs")
+      next
+    # print separators to make scanning easier
+    print("---------------------------------------------------------------------")
+    print(paste(":::::::", rev_depends_ureq[i], ":::::::::::::::::::::::::::::::"))
+    if (!require(rev_depends_ureq[i], character.only = TRUE))
+    {
+      install.packages(rev_depends_ureq[i])
+    }
+  }
+}
+rev_dep_req_install(lhs_rev_depends_req)
+rev_dep_req_install(lhs_rev_imports_req)
+rev_dep_req_install(lhs_rev_suggests_req)
+
+# ensure you start in the package director
+getwd()
+revdepcheck::revdep_reset()
+revdepcheck::revdep_check(num_workers = 4)
--- rev_dep_check.sh
+++ rev_dep_check.sh
@@ -12,6 +12,7 @@
 # sudo apt-get install libcgal-dev libglu1-mesa-dev libglu1-mesa-dev
 # sudo apt-get install libfreetype6-dev
 # sudo apt-get install xorg libx11-dev libftgl2
+# sudo apt-get install libmagick++-dev
 #
 # git clone https://github.com/bertcarnell/lhs
 # cd lhs
@@ -22,63 +23,8 @@
 # getwd() # ensure that the working director is the package directory
 # library(revdepcheck)
 #
-# acebayes
-# install.packages(c("randtoolbox", "R.rsp"))
-# ATmet - pdf issues
-# binaryGP
-# install.packages("GPfit")
-# DoE.wrapper
-# install.packages(c("FrF2", "DoE.base"))
-# DynamicGP - pdf issues
-# EasyABC
-# install.packages("abc")
-# GPfit - pdf issues
-# GPM
-# install.packages(c("randtoolbox","doParallel"))
-# hetGP
-# install.packages("monomvn")
-# inaparc - pdf issues
-# kergp
-# install.packages(c("doParallel", "doFuture"))
-# LVGP
-# install.packages("randtoolbox")
-# maximin
-# install.packages("plgp")
-# netgen - pdf issues
-# optim-functions
-# install.packages(c("randtoolbox", "covr"))
-# install.packages("covr")
-# ParamHelpers - internal test failure
-# install.packages("GGally")
-# penalizedSVM - pdf issues
-# plspolychaos - pdf issues
-# install.packages("polychaosbasics")
-# polychaosbasics - pdf issues
-# profExtrema
-# install.packages(c("KrigInv","pGPx","quantreg","rcdd"))
-# qle
-# install.packages("spatstat")
-# rrepast
-# install.packages(c("rJava","xlsx","doParallel"))
-# spartan
-# install.packages("XML")
-# vdg
-# install.packages("quantreg")
-# grapherator
-# install.packages("vegan")
-# hydroP8O
-# install.packages(c("Hmisc","hydroTSM","hydroGOF","vioplot"))
-# IGP
-# install.packages("PythonInR")
-# ParamHelpers - segfault
-# metagen
-# install.packages(c("BatchJobs","BatchExperiements"))
-# mtk
-# install.packages("rgl")
-# DiceOptim and moko
-# install.packages("GPareto")
-# ParamHelpers
-# install.packages("fastmatch")
+# run the script in rev_dep_check.R
+#
 
 if [ "$(uname -s)" == "MSYS_NT-10.0-WOW" ]; then
   echo Windows Build
